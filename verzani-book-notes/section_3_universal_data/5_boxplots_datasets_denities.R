e86351e4799127153cda3692b1b00adb43fc51fb
Yavor Mihaylov <yavor.st.m@gmail.com>
2019-01-06 11:38:39 +0200

    Add density notes

--- verzani-book-notes/section_3_universal_data/5_boxplots_datasets_denities.R
+++ verzani-book-notes/section_3_universal_data/5_boxplots_datasets_denities.R
@@ -22,3 +22,27 @@ library() # Lists all available packages
 data() # Lists data of all available datasets
 data(package='ts')  # Load the data from this dataset
 lynx
+
+# ----
+x = c(.314,.289,.282,.279,.275,.267,.266,.265,.256,.250,.249,.211,.161)
+tmp = hist(x)
+tmp$breaks
+tmp$mids
+?lines
+lines(
+  c(min(tmp$breaks),tmp$mids,max(tmp$breaks)),
+  c(0,tmp$counts,0),
+  type="l"
+)
+lines(density(x))
+# This is frequency polygon
+# Equivalent is simple.freqpoly.R
+
+# --- Densities
+# define this function
+data('faithful')
+attach(faithful) # make eruptions visible
+hist(eruptions, 15, prob=T) # proportions, not frequencies
+density(eruptions)
+lines(density(eruptions)) # lines makes a curve, default bandwidth
+
